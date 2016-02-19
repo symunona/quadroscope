766ef0ad27b801d2570c6615e65142f7e4928f54
Goose <symunona@gmail.com>
2016-02-19 14:59:07 +0000

    first successful convert

--- quadlib/convert.py
+++ quadlib/convert.py
@@ -7,10 +7,10 @@ def waitForFiles(id, settings):
 	createGif(id, settings)
 
 def createGif(id, settings):	
-	cmd = 'convert -delay 50  -size 400x300 '
-	for i in range(1, settings['cameracount']):
-		cmd += '-page '+ id + '_' + str(i) + '.jpg'
-	cmd += 'loop 0 ' + id + '.gif'
-	
+	cmd = 'convert -delay 50 -size 640x480 -loop 0 '
+	for i in range(0, settings['cameracount']):
+		cmd += settings['uploadpath'] + id + '_' + str(i+1) + '_img.jpg '
+	cmd +=  settings['uploadpath'] + id + '640.gif'
+	print '[convert] ' + cmd
 	os.system(cmd)
 
